5fefe2deb78b9ccb51033b5b162d38c2b1da349b
Xenofon Vourliotis <x.vourliotis@gmail.com>
2022-07-05 18:10:53 +0300

    Change git blame message

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,7 +34,7 @@ local plugins = {
     cond = nocode,
     setup = function()
       vim.g.gitblame_date_format = '%r'
-      vim.g.gitblame_message_template = '<summary> • <date> • <author>'
+      vim.g.gitblame_message_template = '<author>, <date>'
       vim.g.gitblame_display_virtual_text = 0
     end,
   },

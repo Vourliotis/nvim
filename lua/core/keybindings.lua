3c021b8ae657e12d35a1c95eb035cc02270611b3
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2022-10-22 21:15:55 +0300

    Refactor keybindings

---
+++ lua/core/keybindings.lua
@@ -0,0 +1,99 @@
+local bind = vim.keymap.set
+local remap = { remap = true }
+
+vim.opt.langmap = 'ΑA,ΒB,ΨC,ΔD,ΕE,ΦF,ΓG,ΗH,ΙI,ΞJ,ΚK,ΛL,ΜM,ΝN,ΟO,ΠP,QQ,ΡR,ΣS,ΤT,'
+  .. 'ΘU,ΩV,WW,ΧX,ΥY,ΖZ,αa,βb,ψc,δd,εe,φf,γg,ηh,ιi,ξj,κk,λl,μm,νn,οo,πp,qq,ρr,'
+  .. 'σs,τt,θu,ωv,ςw,χx,υy,ζz'
+
+-- Unbind 'K'
+bind('n', 'K', '<NOP>')
+
+-- Exit insert mode
+bind('i', 'jk', '<ESC>')
+bind('i', 'ξκ', '<ESC>')
+
+-- Stay in indent mode after indenting text
+bind('v', '<', '<gv')
+bind('v', '>', '>gv')
+
+-- Move text up and down
+bind('x', 'J', ":move '>+1<CR>gv-gv")
+bind('x', 'K', ":move '<-2<CR>gv-gv")
+
+-- Hop
+bind('n', 's', '<CMD>HopChar2<CR>')
+bind('v', 's', '<CMD>HopChar2<CR>')
+
+-- TUI/GUI NeoVim only settings
+if not vim.g.vscode then
+  vim.g.mapleader = ' '
+
+  -- Clear highlights and prints
+  bind('n', '<leader>n', '<CMD>noh<CR><cmd>echo ""<CR>')
+
+  -- nvim-tree
+  bind('n', '<leader>e', '<CMD>NvimTreeToggle<CR>')
+
+  -- Resize with arrows
+  bind('n', '<C-Up>', '<CMD>resize +2<CR>')
+  bind('n', '<C-Down>', '<CMD>resize -2<CR>')
+  bind('n', '<C-Left>', '<CMD>vertical resize -2<CR>')
+  bind('n', '<C-Right>', '<CMD>vertical resize +2<CR>')
+
+  -- Better window navigation
+  bind('n', '<C-h>', '<C-w>h')
+  bind('n', '<C-j>', '<C-w>j')
+  bind('n', '<C-k>', '<C-w>k')
+  bind('n', '<C-l>', '<C-w>l')
+
+  -- Telescope
+  -- Show key bindings list
+  bind('n', '<Leader>?', '<CMD>Telescope keymaps<CR>')
+  -- Search pattern
+  bind('n', '<leader>fw', '<CMD>Telescope live_grep_args<CR>')
+  -- Find files by name
+  bind('n', '<Leader>ff', '<CMD>Telescope find_files<CR>')
+  bind('n', '<Leader>fa', '<CMD>Telescope find_files follow=true no_ignore=true hidden=true<CR>')
+  -- Search in active buffers list
+  bind('n', '<Leader>fb', '<CMD>Telescope buffers<CR>')
+  -- Resume last search
+  bind('n', '<Leader>fr', '<CMD>Telescope resume<CR>')
+  -- Cached pickers
+  bind('n', '<Leader>fp', '<CMD>Telescope pickers<CR>')
+  -- Help tags
+  bind('n', '<Leader>fh', '<CMD>Telescope help_tags<CR>')
+
+  -- Sessions
+  bind('n', '<Leader>sl', '<CMD>Telescope persisted<CR>')
+  bind('n', '<Leader>ss', '<CMD>SessionSave<CR>')
+
+  -- Bufferline
+  bind('n', '<S-Tab>', '<CMD>BufferPrevious<CR>')
+  bind('n', '<Tab>', '<CMD>BufferNext<CR>')
+  -- Re-order to previous/next
+  bind('n', '<S-h>', '<CMD>BufferMovePrevious<CR>')
+  bind('n', '<S-l>', '<CMD>BufferMoveNext<CR>')
+  -- Pin/unpin buffer
+  bind('n', '<C-p>', '<CMD>BufferPin<CR>')
+  -- Close buffer
+  bind('n', '<C-w>', '<CMD>BufferClose<CR>')
+  bind('n', '<C-s>', '<CMD>BufferPick<CR>')
+
+  -- Git
+  bind('n', '<leader>gd', '<CMD>VGit buffer_diff_preview<CR>')
+  bind('n', '<leader>gh', '<CMD>VGit buffer_history_preview<CR>')
+  bind('n', '<C-]>', '<CMD>VGit hunk_down<CR>')
+  bind('n', '<C-[>', '<CMD>VGit hunk_up<CR>')
+  bind('n', '<leader>gb', '<CMD>VGit toggle_live_blame<CR>')
+
+  -- LSP
+  bind('n', '<leader>k', vim.diagnostic.open_float)
+
+  -- Comments
+  bind('n', '<C-_>', 'gcc', remap)
+  bind('v', '<C-_>', 'gcgv', remap)
+
+  -- Harpoon
+  bind('n', '<leader>hm', '<CMD>lua require("harpoon.mark").add_file()<CR>')
+  bind('n', '<leader>hl', '<CMD>Telescope harpoon marks<CR>')
+end
--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -1,7 +1,7 @@
 local lsp = vim.lsp
 
 local M = {}
-local keymap = vim.keymap.set
+local bind = vim.keymap.set
 
 -- Add borders to hover windows.
 M.hover = lsp.with(lsp.handlers.hover, {
@@ -15,27 +15,24 @@ M.signature_help = lsp.with(lsp.handlers.signature_help, {
 
 -- Custom on attach function.
 M.on_attach = function(client, bufr)
-  -- Mappings.
-  local opts = { noremap = true, silent = true, buffer = bufr }
-  keymap('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  keymap('n', 'gR', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  keymap('i', '<c-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  local opts = { buffer = bufr }
+
+  bind('n', 'ga', lsp.buf.code_action, opts)
+  bind('n', 'gd', lsp.buf.definition, opts)
+  bind('n', 'gD', lsp.buf.declaration, opts)
+  bind('n', 'gi', lsp.buf.implementation, opts)
+  bind('n', 'K', lsp.buf.hover, opts)
+  bind('n', 'gr', lsp.buf.references, opts)
+  bind('n', 'gR', lsp.buf.rename, opts)
+  bind('i', '<c-k>', lsp.buf.signature_help, opts)
 
   -- Formatting is conditional on server capabilities.
   if client.server_capabilities.document_formatting then
-    if vim.fn.has('nvim-0.8') == 1 then
-      keymap('n', "'f", '<cmd>lua vim.lsp.buf.format()<CR>', opts)
-    else
-      keymap('n', "'f", '<cmd>lua vim.lsp.buf.formatting_sync()<CR>', opts)
-    end
+    bind('n', "'f", lsp.buf.format, opts)
   end
+
   if client.server_capabilities.document_range_formatting then
-    keymap('x', "'f", '<cmd>lua vim.lsp.buf.range_formatting()<CR><Esc>', opts)
+    bind('x', "'f", lsp.buf.range_formatting, opts)
   end
 end
 

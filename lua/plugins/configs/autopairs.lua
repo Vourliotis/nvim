7085c2b16d984dc607b530b47ff5be34a1869078
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2022-12-29 19:49:57 +0200

    Refactor `nvim-autopairs` configuration

--- lua/plugins/configs/autopairs.lua
+++ lua/plugins/configs/autopairs.lua
@@ -1,16 +1,19 @@
 local autopairs_present, autopairs = pcall(require, 'nvim-autopairs')
-local cmp_present, cmp = pcall(require, 'cmp')
 
-if not autopairs_present and cmp_present then
+if not autopairs_present then
   return
 end
 
-local options = {
+autopairs.setup({
   fast_wrap = {},
   disable_filetype = { 'TelescopePrompt', 'vim' },
-}
-
-autopairs.setup(options)
+})
 
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp_present, cmp = pcall(require, 'cmp')
+
+if not cmp_present then
+  return
+end
+
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())

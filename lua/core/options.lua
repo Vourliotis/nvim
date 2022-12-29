09e9b0c766a3488da4816234b28f9a288aa0da1a
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2022-12-29 18:10:53 +0200

    Remove default `Ruby` indenting

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -46,6 +46,7 @@ vim.g.loaded_perl_provider = 0
 
 vim.cmd('autocmd FileType * set formatoptions-=cro')
 vim.cmd('autocmd FileType eruby setlocal indentexpr=')
+vim.cmd('autocmd FileType ruby setlocal indentexpr=')
 vim.cmd('set whichwrap+=<,>,[,],h,l')
 vim.cmd('set iskeyword+=-')
 vim.cmd([[

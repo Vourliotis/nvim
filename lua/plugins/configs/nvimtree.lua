129f18d685b63f65978f20d0c56fb5ff4631a037
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-02-10 11:21:22 +0200

    Remove default options from `nvim-tree` configuration

--- lua/plugins/configs/nvimtree.lua
+++ lua/plugins/configs/nvimtree.lua
@@ -5,75 +5,21 @@ if not present then
 end
 
 local options = {
-  filters = {
-    dotfiles = false,
-    exclude = { 'custom' },
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  ignore_ft_on_setup = { 'alpha' },
-  open_on_tab = false,
-  hijack_cursor = true,
-  hijack_unnamed_buffer_when_opening = false,
-  update_cwd = true,
   update_focused_file = {
     enable = true,
-    update_cwd = false,
+    update_root = true,
   },
   view = {
-    side = 'left',
     width = 25,
     hide_root_folder = true,
     adaptive_size = true,
   },
   git = {
     enable = false,
-    ignore = true,
-  },
-  actions = {
-    open_file = {
-      resize_window = true,
-    },
   },
   renderer = {
-    highlight_git = false,
-    highlight_opened_files = 'none',
-
     indent_markers = {
       enable = false,
-    },
-    icons = {
-      padding = ' ',
-      symlink_arrow = ' ➛ ',
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = false,
-      },
-      glyphs = {
-        default = '',
-        symlink = '',
-        folder = {
-          default = '',
-          empty = '',
-          empty_open = '',
-          open = '',
-          symlink = '',
-          symlink_open = '',
-          arrow_open = '',
-          arrow_closed = '',
-        },
-        git = {
-          unstaged = '✗',
-          staged = '✓',
-          unmerged = '',
-          renamed = '➜',
-          untracked = '★',
-          deleted = '',
-          ignored = '◌',
-        },
-      },
     },
   },
   diagnostics = {

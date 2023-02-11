4a8c574342ff85296d1a78b5f8edc50790dc7ba7
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2023-02-11 14:52:04 +0200

    Refactor `indent-blankline.nvim` config

--- lua/plugins/configs/blankline.lua
+++ lua/plugins/configs/blankline.lua
@@ -1,28 +1,22 @@
-local present, blankline = pcall(require, 'indent_blankline')
+local success, blankline = pcall(require, 'indent_blankline')
 
-if not present then
+if not success then
   return
 end
 
-local options = {
-  indentLine_enabled = 1,
+blankline.setup({
   char = '▏',
   context_char = '▏',
   filetype_exclude = {
     'help',
     'terminal',
     'alpha',
-    'packer',
     'lspinfo',
     'TelescopePrompt',
     'TelescopeResults',
     'lsp-installer',
-    '',
   },
-  buftype_exclude = { 'terminal' },
   show_trailing_blankline_indent = false,
   show_first_indent_level = false,
   show_current_context = true,
-}
-
-blankline.setup(options)
+})

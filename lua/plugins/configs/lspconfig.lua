eb413a5fb955c2fddc0e5f0f67542db21fdb7f43
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-02-11 15:24:41 +0200

    Update LSP configuration
    This commit changes the LSP config file structure,
    making it more readable and clear where each setting
    should live.

---
+++ lua/plugins/configs/lspconfig.lua
@@ -0,0 +1,47 @@
+local success, lspconfig = pcall(require, 'lspconfig')
+
+if not success then
+  return
+end
+
+local handlers = require('lsp.handlers')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('mason-lspconfig').setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup({
+      on_attach = handlers.on_attach,
+      capabilities = capabilities,
+    })
+  end,
+})
+
+-- Modified language server settings
+lspconfig.solargraph.setup({
+  on_attach = handlers.on_attach_no_formatting,
+  settings = { solargraph = { diagnostics = false } },
+})
+
+lspconfig.tsserver.setup({
+  on_attach = handlers.on_attach_no_formatting,
+})
+
+lspconfig.sumneko_lua.setup({
+  on_attach = handlers.on_attach_no_formatting,
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    },
+  },
+})
+
+lspconfig.emmet_ls.setup({
+  on_attach = handlers.on_attach_no_formatting,
+  filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'eruby' },
+})
+
+lspconfig.html.setup({
+  filetypes = { 'html', 'eruby' },
+})

807c8c9991d0d109541d287f62636a8dbe1d1dd0
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2023-02-11 16:03:49 +0200

    Replace `Packer.nvim` with `lazy.nvim`
    This commit changes the package manager from
    `Packer.nvim` to `lazy.nvim`. Lazy comes with
    many improvements over Packer in terms of
    performance and ease of use.
    
    More information can be found here:
    https://github.com/folke/lazy.nvim

---
+++ lua/core/lazy.lua
@@ -0,0 +1,37 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = 'plugins',
+  defaults = { lazy = true, version = '*' },
+  install = {},
+  checker = { enabled = true },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'matchit',
+        'matchparen',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
+
+vim.keymap.set('n', '<LEADER>l', '<CMD>Lazy<CR>')
---
+++ lua/plugins/git.lua
@@ -0,0 +1,25 @@
+local vscode = require('core.utils').vscode
+
+return {
+  {
+    'akinsho/git-conflict.nvim',
+    cond = not vscode,
+    config = function()
+      require('git-conflict').setup()
+    end,
+  },
+  {
+    'tanvirtin/vgit.nvim',
+    cond = not vscode,
+    keys = {
+      { '<LEADER>gd', '<CMD>VGit buffer_diff_preview<CR>', mode = 'n' },
+      { '<LEADER>gh', '<CMD>VGit buffer_history_preview<CR>', mode = 'n' },
+      { '<C-]>', '<CMD>VGit hunk_down<CR>', mode = 'n' },
+      { '<C-[>', '<CMD>VGit hunk_up<CR>', mode = 'n' },
+      { '<LEADER>gb', '<CMD>VGit toggle_live_blame<CR>', mode = 'n' },
+    },
+    config = function()
+      require('plugins.configs.vgit')
+    end,
+  },
+}

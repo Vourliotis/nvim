1c29967dc0b1de3d5a166dd5ebd6ac00bee553b7
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2023-02-11 12:17:50 +0200

    Refactor `toggleterm.nvim` config

--- lua/plugins/configs/toggleterm.lua
+++ lua/plugins/configs/toggleterm.lua
@@ -1,22 +1,12 @@
-local status_ok, toggleterm = pcall(require, 'toggleterm')
+local success, toggleterm = pcall(require, 'toggleterm')
 
-if not status_ok then
+if not success then
   return
 end
 
 toggleterm.setup({
-  size = 20,
   open_mapping = [[<c-\>]],
-  hide_numbers = true,
-  shade_filetypes = {},
-  shade_terminals = true,
-  shading_factor = 2,
-  start_in_insert = true,
-  insert_mappings = true,
-  persist_size = true,
   direction = 'float',
-  close_on_exit = true,
-  shell = vim.o.shell,
   float_opts = {
     border = 'curved',
     winblend = 0,

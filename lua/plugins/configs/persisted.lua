e6ead6b52399b15666c4e3eeea8c62dc0b44cfd9
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2023-02-11 14:43:20 +0200

    Refactor `persisted.nvim` config

--- lua/plugins/configs/persisted.lua
+++ lua/plugins/configs/persisted.lua
@@ -1,26 +1,26 @@
-require('persisted').setup({
-  save_dir = vim.fn.expand(vim.fn.stdpath('data') .. '/sessions/'),
-  command = 'VimLeavePre',
+local success, persisted = pcall(require, 'persisted')
+
+if not success then
+  return
+end
+
+persisted.setup({
   use_git_branch = true,
   autosave = false,
-  autoload = false,
-  allowed_dirs = nil,
-  ignored_dirs = nil,
-  before_save = nil,
   after_save = function()
-    print("Session was saved!")
+    print('Session saved!')
   end,
   after_source = function(session)
-    print("Loaded session " .. session.name)
+    print('Loaded session ' .. session.name)
   end,
   telescope = {
     before_source = function()
       -- Close all open buffers
       -- Thanks to https://github.com/avently
-      vim.api.nvim_input("<ESC>:%bd<CR>")
+      vim.api.nvim_input('<ESC>:%bd<CR>')
     end,
     after_source = function(session)
-      print("Loaded session " .. session.name)
+      print('Loaded session ' .. session.name)
     end,
   },
 })

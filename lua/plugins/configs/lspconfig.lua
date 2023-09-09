43949c51840c0a95f49bfd9848ce9dfe8e7526f0
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-09-10 00:24:30 +0300

    Broadcast capabilities to all LSP servers

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -1,56 +1,54 @@
-local success, lspconfig = pcall(require, 'lspconfig')
+local lspconfig_success, lspconfig = pcall(require, 'lspconfig')
+local mason_lspconfig_success, mason_lspconfig = pcall(require, 'mason-lspconfig')
 
-if not success then
+if not (lspconfig_success and mason_lspconfig_success) then
   return
 end
 
 local handlers = require('lsp.handlers')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-capabilities.textDocument.foldingRange = {
-  dynamicRegistration = false,
-  lineFoldingOnly = true,
-}
 
-require('mason-lspconfig').setup_handlers({
-  function(server_name)
-    lspconfig[server_name].setup({
-      on_attach = handlers.on_attach,
-      capabilities = capabilities,
-    })
-  end,
-  ['lua_ls'] = function()
-    lspconfig.sumneko_lua.setup({
-      on_attach = handlers.on_attach_no_formatting,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' },
-          },
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
+
+local servers = {
+  lua_ls = {
+    on_attach = handlers.on_attach_no_formatting,
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { 'vim' },
         },
       },
-    })
-  end,
-  ['solargraph'] = function()
-    lspconfig.solargraph.setup({
-      on_attach = handlers.on_attach_no_formatting,
-      settings = { solargraph = { diagnostics = false } },
-    })
-  end,
-  ['tsserver'] = function()
-    lspconfig.tsserver.setup({
-      on_attach = handlers.on_attach_no_formatting,
-    })
-  end,
-  ['emmet_ls'] = function()
-    lspconfig.emmet_ls.setup({
-      on_attach = handlers.on_attach_no_formatting,
-      filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'eruby' },
-    })
-  end,
-  ['html'] = function()
-    lspconfig.html.setup({
-      filetypes = { 'html', 'eruby' },
+    },
+  },
+  solargraph = {
+    on_attach = handlers.on_attach_no_formatting,
+    settings = {
+      solargraph = {
+        diagnostics = false,
+      },
+    },
+  },
+  tsserver = {
+    on_attach = handlers.on_attach_no_formatting,
+  },
+  emmet_ls = {
+    on_attach = handlers.on_attach_no_formatting,
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'eruby' },
+  },
+  html = {
+    filetypes = { 'html', 'eruby' },
+  },
+}
+
+mason_lspconfig.setup_handlers({
+  function(server_name)
+    lspconfig[server_name].setup({
+      capabilities = capabilities,
+      on_attach = (servers[server_name] or {}).on_attach or handlers.on_attach,
+      settings = (servers[server_name] or {}).settings,
+      filetypes = (servers[server_name] or {}).filetypes,
     })
   end,
 })

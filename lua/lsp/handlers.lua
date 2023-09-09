a1aaf1deeb3df52f823cb019274f69b373e4a757
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-09-10 01:13:33 +0300

    Update LSP keybindings

--- lua/lsp/handlers.lua
+++ lua/lsp/handlers.lua
@@ -7,13 +7,17 @@ M.on_attach = function(_, bufr)
   local opts = { buffer = bufr }
 
   bind('n', "<LEADER>'f", lsp.buf.format, opts)
-  bind('n', 'ga', lsp.buf.code_action, opts)
+  bind('n', '<LEADER>ca', lsp.buf.code_action, opts)
+  bind('n', '<LEADER>rn', lsp.buf.rename, opts)
+  bind('n', '<LEADER>td', lsp.buf.type_definition, opts)
+  bind('n', '<LEADER>ds', require('telescope.builtin').lsp_document_symbols, opts)
+  bind('n', '<LEADER>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, opts)
+
   bind('n', 'gd', lsp.buf.definition, opts)
+  bind('n', 'gr', require('telescope.builtin').lsp_references, opts)
+  bind('n', 'gi', require('telescope.builtin').lsp_implementations, opts)
   bind('n', 'gD', lsp.buf.declaration, opts)
-  bind('n', 'gi', lsp.buf.implementation, opts)
   bind('n', 'K', lsp.buf.hover, opts)
-  bind('n', 'gr', lsp.buf.references, opts)
-  bind('n', 'gR', lsp.buf.rename, opts)
   bind('i', '<c-k>', lsp.buf.signature_help, opts)
 end
 

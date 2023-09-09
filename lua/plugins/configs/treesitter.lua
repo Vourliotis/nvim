4ebae85269e7c40c5c0d4cb88dc60768bb61d902
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-09-10 01:39:24 +0300

    Add `nvim-treesitter-textobjects`

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -32,4 +32,53 @@ treesitter.setup({
       node_incremental = '<c-space>',
     },
   },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['aa'] = '@parameter.outer',
+        ['ia'] = '@parameter.inner',
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ab'] = '@block.outer',
+        ['ib'] = '@block.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']f'] = '@function.outer',
+        [']b'] = '@block.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']F'] = '@function.outer',
+        [']B'] = '@block.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[f'] = '@function.outer',
+        ['[b'] = '@block.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[F'] = '@function.outer',
+        ['[B'] = '@block.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+    swap = {
+      enable = true,
+      swap_next = {
+        ['<leader>a'] = '@parameter.inner',
+      },
+      swap_previous = {
+        ['<leader>A'] = '@parameter.inner',
+      },
+    },
+  },
 })

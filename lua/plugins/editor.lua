76bf2d2b9912d24d31578926b7b12c8f8c6c6c53
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2023-09-25 12:09:33 +0300

    Disable `flash.nvim` during regular search

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -4,7 +4,13 @@ return {
   {
     'folke/flash.nvim',
     event = 'VeryLazy',
-    opts = {},
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
     keys = {
       {
         's',

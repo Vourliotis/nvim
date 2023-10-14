7098ad2e139fab879f39b9171abc69d0afc6f51e
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-10-14 15:10:27 +0300

    Add `nvim-lint` for linting

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -35,6 +35,27 @@ return {
     },
   },
   {
+    'mfussenegger/nvim-lint',
+    cond = not vscode,
+    event = { 'BufReadPost', 'BufNewFile', 'BufWritePre' },
+    opts = {
+      events = { 'BufWritePost', 'BufReadPost', 'InsertLeave' },
+      linters_by_ft = {
+        css = { 'stylelint' },
+        scss = { 'stylelint' },
+      },
+    },
+    config = function(_, opts)
+      require('lint').linters_by_ft = opts.linters_by_ft
+
+      vim.api.nvim_create_autocmd(opts.events, {
+        callback = function()
+          require('lint').try_lint()
+        end,
+      })
+    end,
+  },
+  {
     'numToStr/Comment.nvim',
     cond = not vscode,
     keys = {

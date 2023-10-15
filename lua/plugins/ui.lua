2195621695f03c7035b93309455bffa31c3f1137
Xenofon Vourliotis <x.vourliotis@gmail.com>
2023-10-15 13:42:28 +0300

    Add `noice.nvim`

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -99,4 +99,25 @@ return {
       })
     end,
   },
+  {
+    'folke/noice.nvim',
+    cond = not vscode,
+    event = 'VeryLazy',
+    opts = {
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        long_message_to_split = true,
+      },
+    },
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+  },
 }

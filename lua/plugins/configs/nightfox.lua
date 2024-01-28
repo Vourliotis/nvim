cc54fd7133d6aecf0278f485c2e2d6a4bf8d6856
Xenofon Vourliotis <xvourliotis@skroutz.gr>
2024-01-28 19:18:44 +0200

    Follow Treesitter upstream highlight groups

--- lua/plugins/configs/nightfox.lua
+++ lua/plugins/configs/nightfox.lua
@@ -1,8 +1,6 @@
 local groups = {
   nightfox = {
-    ['@label'] = { fg = 'palette.orange.bright' },
-    ['@symbol'] = { fg = 'palette.blue' },
-    ['@type'] = { fg = 'palette.yellow.bright' },
+    ['@variable.member'] = { fg = 'palette.orange.bright' },
     BufferCurrent = { bg = 'palette.bg1', style = 'bold' },
     BufferCurrentMod = { bg = 'palette.bg1', style = 'bold' },
     StatusLine = { bg = 'palette.bg2' },

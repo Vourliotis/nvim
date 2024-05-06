1d62d5d4e61ac3c33f36207afdff6ec4403f88e1
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-05-06 16:24:52 +0300

    Update `nightfox.nvim` color palette

--- lua/plugins/configs/nightfox.lua
+++ lua/plugins/configs/nightfox.lua
@@ -1,3 +1,14 @@
+local palettes = {
+  nightfox = {
+    bg0 = '#1e2030',
+    bg1 = '#24273a',
+    bg2 = '#363a4f',
+    bg3 = '#3e445e',
+    bg4 = '#4c5373',
+    sel0 = '#3e445e',
+  },
+}
+
 local groups = {
   nightfox = {
     ['@variable.member'] = { fg = 'palette.orange.bright' },
@@ -8,4 +19,4 @@ local groups = {
   },
 }
 
-require('nightfox').setup({ groups = groups })
+require('nightfox').setup({ palettes = palettes, groups = groups })

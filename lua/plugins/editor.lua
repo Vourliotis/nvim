5b7ef0ced221fa8f94ea75348423618d7bb44cdc
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-05-07 15:54:21 +0300

    Add `mini.bufremove` to handle buffer removing

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -82,4 +82,37 @@ return {
       { '<leader>dw', '<cmd>TroubleToggle workspace_diagnostics<cr>' },
     },
   },
+  {
+    'echasnovski/mini.bufremove',
+    cond = not vscode,
+    keys = {
+      {
+        '<C-w>',
+        function()
+          local bd = require('mini.bufremove').delete
+
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(('Save changes to %q?'):format(vim.fn.bufname()), '&Yes\n&No\n&Cancel')
+
+            -- Yes
+            if choice == 1 then
+              vim.cmd.write()
+              bd(0)
+            -- No
+            elseif choice == 2 then
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
+        end,
+      },
+      {
+        '<A-w>',
+        function()
+          require('mini.bufremove').delete(0, true)
+        end,
+      },
+    },
+  },
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -42,8 +42,6 @@ return {
       { '<S-h>', '<CMD>BufferMovePrevious<CR>', mode = 'n' },
       { '<S-l>', '<CMD>BufferMoveNext<CR>', mode = 'n' },
       { '<C-p>', '<CMD>BufferPin<CR>', mode = 'n' },
-      { '<C-w>', '<CMD>BufferClose<CR>', mode = 'n' },
-      { '<A-w>', '<CMD>BufferClose!<CR>', mode = 'n' },
       { '<C-s>', '<CMD>BufferPick<CR>', mode = 'n' },
     },
     opts = {

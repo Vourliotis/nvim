515725fbcf0e6437ff4b5fe48da19daac3affb31
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-05-18 11:42:35 +0300

    Disable python provider

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -85,6 +85,7 @@ vim.opt.foldenable = true
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
 
 -- Clear highlighting for ColorColumn
 vim.cmd('highlight clear ColorColumn')

d229c706f635919829b4809ac9a3a0a4466ef4e2
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-05-19 01:11:45 +0300

    Update core utility functions

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,14 +1,42 @@
 local M = {}
 
----@param on_attach fun(client, buffer)
+---@param on_attach fun(client:table, buffer:number)
 function M.on_attach(on_attach)
-  vim.api.nvim_create_autocmd("LspAttach", {
+  vim.api.nvim_create_autocmd('LspAttach', {
     callback = function(args)
       local buffer = args.buf
       local client = vim.lsp.get_client_by_id(args.data.client_id)
       on_attach(client, buffer)
     end,
   })
+end
+
+---@param name string
+---@param fn fun(name:string)
+function M.on_load(name, fn)
+  local config = require('lazy.core.config')
+  if config.plugins[name] and config.plugins[name]._.loaded then
+    fn(name)
+  else
+    vim.api.nvim_create_autocmd('User', {
+      pattern = 'LazyLoad',
+      callback = function(event)
+        if event.data == name then
+          fn(name)
+          return true
+        end
+      end,
+    })
+  end
+end
+
+--- Merge extended options with a default table of options
+---@param default? table The default table that you want to merge into
+---@param opts? table The new options that should be merged with the default table
+---@return table # The merged table
+function M.extend_tbl(default, opts)
+  opts = opts or {}
+  return default and vim.tbl_deep_extend('force', default, opts) or opts
 end
 
 return M

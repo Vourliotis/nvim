a1e0eb228da656091f889f11be2d38d565bb2109
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-06-14 10:58:21 +0300

    Add `faster.nvim` to handle large files

---
+++ lua/plugins/faster.lua
@@ -0,0 +1,8 @@
+return {
+  'pteroctopus/faster.nvim',
+  keys = {
+    { '<LEADER>fd', '<CMD>FasterDisableAllFeatures<CR>', mode = 'n' },
+    { '<LEADER>fe', '<CMD>FasterEnableAllFeatures<CR>', mode = 'n' },
+  },
+  opts = {},
+}

540bda2cd0e2fa9b43ad0bf5667d3ac113ea3960
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-09-17 13:19:58 +0300

    Enable async formatting with `conform.nvim`

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,7 +7,7 @@ return {
     {
       "<leader>'f",
       function()
-        require('conform').format({ lsp_fallback = true })
+        require('conform').format({ async = true, lsp_fallback = true })
       end,
       mode = 'n',
     },

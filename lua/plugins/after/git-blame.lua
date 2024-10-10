e10b1e22c9754e755e7384f9d3d916ec0f6b72f6
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-10-10 10:46:07 +0300

    Update schedule event for git blame

--- lua/plugins/after/git-blame.lua
+++ lua/plugins/after/git-blame.lua
@@ -7,6 +7,7 @@ return {
     config = function()
       vim.g.gitblame_display_virtual_text = 0
       vim.g.gitblame_date_format = '%r'
+      vim.g.gitblame_schedule_event = 'CursorHold'
     end,
   },
   {

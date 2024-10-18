a67835e533dae07302735df5bcb249900626cbdf
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-10-18 11:26:24 +0300

    Add send to quickfix list keybind for `live_grep_args`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,6 +27,7 @@ return {
                     ['<C-h>'] = lga_actions.quote_prompt({ postfix = ' --iglob ' }),
                     ['<C-l>'] = lga_actions.quote_prompt({ postfix = " --iglob '!spec' --iglob " }),
                     ['<C-Space>'] = actions.to_fuzzy_refine,
+                    ['<C-q>'] = actions.send_to_qflist,
                   },
                 },
               },

946e22759c5a691bf09bd2aceec25b3e97f42bd1
Xenofon Vourliotis <x.vourliotis@gmail.com>
2024-12-24 00:15:16 +0200

    Add `mini.trailspace` to handle trailing whitespace and empty lines

---
+++ lua/plugins/mini-trailspace.lua
@@ -0,0 +1,30 @@
+return {
+  'echasnovski/mini.trailspace',
+  version = '*',
+  keys = {
+    {
+      '<LEADER>tl',
+      function()
+        local MiniTrailspace = require('mini.trailspace')
+        MiniTrailspace.trim()
+        MiniTrailspace.trim_last_lines()
+      end,
+      mode = 'n',
+    },
+  },
+  init = function()
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      pattern = '*',
+      callback = function()
+        if vim.bo.filetype == 'oil' then
+          return
+        end
+
+        local MiniTrailspace = require('mini.trailspace')
+        MiniTrailspace.trim()
+        MiniTrailspace.trim_last_lines()
+      end,
+    })
+  end,
+  opts = {},
+}

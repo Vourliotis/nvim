591616694bffc0291bd6873e32af02e6364e9b20
Xenofon Vourliotis <x.vourliotis@gmail.com>
2025-04-26 23:14:40 +0100

    Update `fzf-lua.nvim` styling

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,29 +1,32 @@
 return {
-  'ibhagwan/fzf-lua',
-  keys = {
-    { '<LEADER>ff', '<CMD>FzfLua files<CR>', mode = 'n' },
-    { '<LEADER>fp', '<CMD>FzfLua resume<CR>', mode = 'n' },
-    { '<LEADER>fw', '<CMD>FzfLua live_grep_glob<CR>', mode = 'n' },
-    { '<LEADER>fb', '<CMD>FzfLua buffers<CR>', mode = 'n' },
-    { '<LEADER>fr', '<CMD>FzfLua live_grep_resume<CR>', mode = 'n' },
-    { '<LEADER>fz', '<CMD>FzfLua<CR>', mode = 'n' },
-  },
-  opts = {
-    fzf_colors = {
-      true,
-      ['fg'] = { 'fg', 'CursorLine' },
-      ['bg'] = { 'bg', 'Normal' },
-      ['hl'] = { 'fg', 'Comment' },
-      ['fg+'] = { 'fg', 'Normal', 'underline' },
-      ['bg+'] = { 'bg', { 'CursorLine', 'Normal' } },
-      ['hl+'] = { 'fg', 'Statement' },
-      ['info'] = { 'fg', 'PreProc' },
-      ['prompt'] = { 'fg', 'Conditional' },
-      ['pointer'] = { 'fg', 'Exception' },
-      ['marker'] = { 'fg', 'Keyword' },
-      ['spinner'] = { 'fg', 'Label' },
-      ['header'] = { 'fg', 'Comment' },
-      ['gutter'] = '-1',
+  {
+    'ibhagwan/fzf-lua',
+    keys = {
+      { '<LEADER>ff', '<CMD>FzfLua files<CR>', mode = 'n' },
+      { '<LEADER>fp', '<CMD>FzfLua resume<CR>', mode = 'n' },
+      { '<LEADER>fw', '<CMD>FzfLua live_grep_glob<CR>', mode = 'n' },
+      { '<LEADER>fb', '<CMD>FzfLua buffers<CR>', mode = 'n' },
+      { '<LEADER>fr', '<CMD>FzfLua live_grep_resume<CR>', mode = 'n' },
+      { '<LEADER>fz', '<CMD>FzfLua<CR>', mode = 'n' },
     },
+    opts = {
+      winopts = { backdrop = 100 },
+      files = { previewer = false },
+      fzf_colors = true,
+      fzf_opts = { ['--border'] = false },
+    },
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    optional = true,
+    opts = function(_, opts)
+      return require('core.utils').extend_tbl(opts, {
+        groups = {
+          nightfox = {
+            FzfLuaBorder = { fg = 'palette.bg3' },
+          },
+        },
+      })
+    end,
   },
 }

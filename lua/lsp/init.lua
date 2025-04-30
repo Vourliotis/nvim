e8b502470370c42047a60d6396b7166fbcf15fef
Xenofon Vourliotis <x.vourliotis@gmail.com>
2025-04-30 01:01:40 +0100

    Update LSP configuration to new LSP nvim API

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,21 +1,96 @@
--- Diagnostics floatic window
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = true,
-  float = {
-    border = 'single',
+  severity_sort = true,
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  float = { border = 'single', source = 'if_many' },
+  signs = vim.g.have_nerd_font and {
+    text = {
+      [vim.diagnostic.severity.ERROR] = '󰅚 ',
+      [vim.diagnostic.severity.WARN] = '󰀪 ',
+      [vim.diagnostic.severity.INFO] = '󰋽 ',
+      [vim.diagnostic.severity.HINT] = '󰌶 ',
+    },
+  } or {},
+  virtual_text = {
+    source = 'if_many',
+    spacing = 2,
     format = function(diagnostic)
-      return string.format('%s (%s) [%s]', diagnostic.message, diagnostic.source, diagnostic.code)
+      local diagnostic_message = {
+        [vim.diagnostic.severity.ERROR] = diagnostic.message,
+        [vim.diagnostic.severity.WARN] = diagnostic.message,
+        [vim.diagnostic.severity.INFO] = diagnostic.message,
+        [vim.diagnostic.severity.HINT] = diagnostic.message,
+      }
+      return diagnostic_message[diagnostic.severity]
     end,
   },
 })
 
--- Add borders to hover windows.
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = 'single',
-})
+local open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+  opts = opts or {}
+  opts.border = opts.border or 'single'
 
--- Add borders to signature help windows.
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-  border = 'single',
+  return open_floating_preview(contents, syntax, opts, ...)
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
+  callback = function(event)
+    local bind = function(keys, func, mode)
+      mode = mode or 'n'
+      vim.keymap.set(mode, keys, func, { buffer = event.buf })
+    end
+
+    bind('gn', vim.lsp.buf.rename)
+    bind('gca', vim.lsp.buf.code_action, { 'n', 'x' })
+    bind('gr', require('telescope.builtin').lsp_references)
+    bind('gi', require('telescope.builtin').lsp_implementations)
+    bind('gd', require('telescope.builtin').lsp_definitions)
+    bind('gD', vim.lsp.buf.declaration)
+    bind('gs', require('telescope.builtin').lsp_document_symbols)
+    bind('gw', require('telescope.builtin').lsp_dynamic_workspace_symbols)
+    bind('gt', require('telescope.builtin').lsp_type_definitions)
+    bind('K', vim.lsp.buf.hover)
+    bind('<C-i>', vim.lsp.buf.signature_help, 'i')
+
+    local function client_supports_method(client, method, bufnr)
+      if vim.fn.has('nvim-0.11') == 1 then
+        return client:supports_method(method, bufnr)
+      else
+        return client.supports_method(method, { bufnr = bufnr })
+      end
+    end
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if
+      client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf)
+    then
+      local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds({ buffer = event2.buf })
+        end,
+      })
+    end
+
+    if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+      bind('<leader>th', function()
+        vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+      end)
+    end
+  end,
 })
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,20 +1,8 @@
 return {
   'neovim/nvim-lspconfig',
-  dependencies = {
-    { 'williamboman/mason.nvim', opts = {} },
-    'williamboman/mason-lspconfig.nvim',
-    { 'WhoIsSethDaniel/mason-tool-installer.nvim', opts = {} },
-  },
   config = function()
-    local lspconfig = require('lspconfig')
-    local mason_lspconfig = require('mason-lspconfig')
-
-    local handlers = require('lsp.handlers')
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-    capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
-
-    local servers = {
+    local server_configurations = {
       lua_ls = {
         settings = {
           Lua = {
@@ -24,28 +12,17 @@ return {
           },
         },
       },
-      html = {
-        filetypes = { 'html', 'eruby' },
-      },
-      rubocop = {
-        -- The lspconfig of rubocop runs `bundle exec` in front of the cmd by
-        -- default when it detects a Gemfile, and as a result does not use the
-        -- executable installed by Mason. This overwrites that behaviour.
-        on_new_config = function(config, _)
-          config.cmd = { 'rubocop', '--lsp' }
-        end,
-      },
     }
 
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        local default_config = {
-          capabilities = capabilities,
-          on_attach = handlers.on_attach,
-        }
+    local servers = vim.tbl_keys(server_configurations or {})
+    vim.list_extend(servers, { 'stylua' })
 
-        lspconfig[server_name].setup(vim.tbl_extend('force', default_config, servers[server_name] or {}))
-      end,
-    })
+    for _, server in ipairs(servers) do
+      local server_configuration = server_configurations[server] or {}
+      server_configuration.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+
+      vim.lsp.enable(server)
+      vim.lsp.config(server, server_configuration)
+    end
   end,
 }
